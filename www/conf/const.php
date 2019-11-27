82d1973e31e1d5bb2b809d32e091522ea8d5eac4
yuzaemon <iret5edmk@gmail.com>
2019-11-27 21:24:26 +0900

    change const.php

--- www/conf/const.php
+++ www/conf/const.php
@@ -1,51 +1,12 @@
 <?php
 
-define('MODEL_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../model/');
-define('VIEW_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../view/');
+// データベースの接続情報
+define('DB_USER', 'codecamp29105');
+define('DB_PASSWD', 'PPLEPMZM');
+define('DB_NAME', 'codecamp29105');
+define('DSN', 'mysql:dbname='.DB_NAME.';host=localhost;charset=utf8');
 
-
-define('IMAGE_PATH', '/assets/images/');
-define('STYLESHEET_PATH', '/assets/css/');
-define('IMAGE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets/images/' );
-
-define('DB_HOST', 'mysql');
-define('DB_NAME', 'sample');
-define('DB_USER', 'testuser');
-define('DB_PASS', 'password');
-define('DB_CHARSET', 'utf8');
-
-define('SIGNUP_URL', '/signup.php');
-define('LOGIN_URL', '/login.php');
-define('LOGOUT_URL', '/logout.php');
-define('HOME_URL', '/index.php');
-define('CART_URL', '/cart.php');
-define('FINISH_URL', '/finish.php');
-define('ADMIN_URL', '/admin.php');
-
-define('REGEXP_ALPHANUMERIC', '/\A[0-9a-zA-Z]+\z/');
-define('REGEXP_POSITIVE_INTEGER', '/\A([1-9][0-9]*|0)\z/');
-
-
-define('USER_NAME_LENGTH_MIN', 6);
-define('USER_NAME_LENGTH_MAX', 100);
-define('USER_PASSWORD_LENGTH_MIN', 6);
-define('USER_PASSWORD_LENGTH_MAX', 100);
-
-define('USER_TYPE_ADMIN', 1);
-define('USER_TYPE_NORMAL', 2);
-
-define('ITEM_NAME_LENGTH_MIN', 1);
-define('ITEM_NAME_LENGTH_MAX', 100);
-
-define('ITEM_STATUS_OPEN', 1);
-define('ITEM_STATUS_CLOSE', 0);
-
-define('PERMITTED_ITEM_STATUSES', array(
-  'open' => 1,
-  'close' => 0,
-));
-
-define('PERMITTED_IMAGE_TYPES', array(
-  IMAGETYPE_JPEG => 'jpg',
-  IMAGETYPE_PNG => 'png',
-));
+// データベースのDNS情報
+define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
+define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
+define('IMG_DIR', './img/');   // 画像ファイル保存ディレクトリ
